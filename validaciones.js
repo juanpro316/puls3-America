480fe73b30d54bfae465b85a4aebb9c036542488
juanpro316 <juan@bisolutions.mx>
2016-03-05 16:27:22 -0800

    finales

---
+++ validaciones.js
@@ -0,0 +1,17 @@
+var valoracion;
+valoracion=0;
+
+function up
+{
+	for (valoracion=0;valoracion+up;valoracion++) 
+	{
+		valoracion++;
+	}
+}
+function down
+{
+	for (valoracion=0;valoracion+down;valoracion--) 
+	{
+		valoracion--;
+	}
+}
